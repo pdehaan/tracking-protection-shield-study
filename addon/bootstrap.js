2c6a14355009fc9abc441bfba0a958876763c0eb
Bianca Danforth <biancadanforth@gmail.com>
2017-12-01 16:49:33 -0800

    Cleanup bootstrap.js some; add UI-ready observer
    UI observer code from @rhelmer at https://github.com/rhelmer/tracking-protection-study/

--- addon/bootstrap.js
+++ addon/bootstrap.js
@@ -7,7 +7,6 @@
 //TODO bdanforth: Fix bootstrap log; submit patch to ssat
 
 const { classes: Cc, interfaces: Ci, utils: Cu } = Components;
-Cu.import("resource://gre/modules/Console.jsm");
 Cu.import("resource://gre/modules/Log.jsm");
 Cu.import("resource://gre/modules/Services.jsm");
 Cu.import("resource://gre/modules/XPCOMUtils.jsm");
@@ -42,69 +41,53 @@ XPCOMUtils.defineLazyModuleGetter(this, "Feature", `resource://${BASE}/lib/Featu
     "resource://gre/modules/Preferences.jsm");
 */
 
+// Create a new instance of the ConsoleAPI so we can control the maxLogLevel.
+XPCOMUtils.defineLazyGetter(this, "log", () => {
+  const ConsoleAPI = Cu.import("resource://gre/modules/Console.jsm", {}).ConsoleAPI;
+  const consoleOptions = {
+    maxLogLevel: config.log.bootstrap.level,
+    prefix: "TPStudy",
+  };
+  return new ConsoleAPI(consoleOptions);
+});
+
 this.Bootstrap = {
   async startup(addonData, reason) {
-    this.setupLog();
-    // `addonData`: Array [ "id", "version", "installPath", "resourceURI", "instanceID", "webExtension" ]  bootstrap.js:48
-    this.log.debug("startup", REASONS[reason] || reason);
 
-    /* Configuration of Study Utils*/
-    studyUtils.setup({
-      ...config,
-      addon: { id: addonData.id, version: addonData.version },
-    });
-    // choose the variation for this particular user, then set it.
-    const variation = this.getVariationFromPref(config.weightedVariations) ||
-      await studyUtils.deterministicVariation(
-        config.weightedVariations
-      );
-    studyUtils.setVariation(variation);
-    this.log.debug(`studyUtils has config and variation.name: ${variation.name}.  Ready to send telemetry`);
+    // validate study config
+    studyUtils.setup({...config, addon: { id: addonData.id, version: addonData.version }});
+    // TODO bdanforth: patch studyUtils to setLoggingLevel as part of setup method
+    studyUtils.setLoggingLevel(config.log.studyUtils.level);
 
-
-    /** addon_install ONLY:
-      * - note first seen,
-      * - check eligible
-      */
-    if ((REASONS[reason]) === "ADDON_INSTALL") {
-      //  telemetry "enter" ONCE
-      studyUtils.firstSeen();
-      const eligible = await config.isEligible(); // addon-specific
-      if (!eligible) {
-        // 1. uses config.endings.ineligible.url if any,
-        // 2. sends UT for "ineligible"
-        // 3. then uninstalls addon
-        await studyUtils.endStudy({reason: "ineligible"});
-        return;
-      }
+    // if addon was just installed, check if user is eligible as specified in Config.jsm
+    if ((REASONS[reason]) === "ADDON_INSTALL" && await !this.isEligible(reason)) {
+      return;
     }
 
-    // startup for eligible users.
-    // 1. sends `install` ping IFF ADDON_INSTALL.
-    // 2. sets activeExperiments in telemetry environment.
+    // choose and set variation
+    const variation = await this.selectVariation();
+
+    /*
+    * Adds the study to the active list of telemetry experiments, and sends the "installed"
+    * telemetry ping if applicable
+    */
     await studyUtils.startup({reason});
 
-    // if you have code to handle expiration / long-timers, it could go here
-    (function fakeTrackExpiration() {})();
+    // log what the study variation and other info is.
+    log.debug(`info ${JSON.stringify(studyUtils.info())}`);
 
-    // IFF your study has an embedded webExtension, start it.
+    // IFF the study has an embedded webExtension, start it.
     const { webExtension } = addonData;
     if (webExtension) {
-      webExtension.startup().then(api => {
-        const {browser} = api;
-        /** spec for messages intended for Shield =>
-          * {shield:true,msg=[info|endStudy|telemetry],data=data}
-          */
-        browser.runtime.onMessage.addListener(studyUtils.respondToWebExtensionMessage);
-        // other browser.runtime.onMessage handlers for your addon, if any
-      });
+      await this.startupWebExtension(webExtension);
     }
 
-    // log what the study variation and other info is.
-    this.log.debug(`info ${JSON.stringify(studyUtils.info())}`);
-
-    // Start up your feature, with specific variation info.
-    this.feature = new Feature({variation, studyUtils, reasonName: REASONS[reason]});
+    // make sure the UI is available before adding the feature
+    if (!Services.wm.getMostRecentWindow("navigator:browser")) {
+      Services.obs.addObserver(this, UI_AVAILABLE_NOTIFICATION);
+    } else {
+      this.addFeature(variation, reason);
+    }
   },
 
   /** Shutdown needs to distinguish between USER-DISABLE and other
@@ -113,13 +96,13 @@ this.Bootstrap = {
   * studyUtils._isEnding means this is a '2nd shutdown'.
   */
   shutdown(addonData, reason) {
-    this.log.debug("shutdown", REASONS[reason] || reason);
+    log.debug("shutdown", REASONS[reason] || reason);
     // FRAGILE: handle uninstalls initiated by USER or by addon
     if (reason === REASONS.ADDON_UNINSTALL || reason === REASONS.ADDON_DISABLE) {
-      this.log.debug("uninstall or disable");
+      log.debug("uninstall or disable");
       if (!studyUtils._isEnding) {
         // we are the first 'uninstall' requestor => must be user action.
-        this.log.debug("probably: user requested shutdown");
+        log.debug("probably: user requested shutdown");
         studyUtils.endStudy({reason: "user-disable"});
         return;
       }
@@ -136,12 +119,64 @@ this.Bootstrap = {
   },
 
   uninstall(addonData, reason) {
-    this.log.debug("uninstall", REASONS[reason] || reason);
+    log.debug("uninstall", REASONS[reason] || reason);
   },
 
   install(addonData, reason) {
-    this.log.debug("install", REASONS[reason] || reason);
+    log.debug("install", REASONS[reason] || reason);
     // handle ADDON_UPGRADE (if needful) here
+  },
+
+  observe(subject, topic, data) {
+    if (topic === UI_AVAILABLE_NOTIFICATION) {
+      Services.obs.removeObserver(this, UI_AVAILABLE_NOTIFICATION);
+      this.onBrowserReady();
+    }
+  },
+
+  addFeature(variation, reason) {
+    // TODO bdanforth: load frame script in Feature.init
+    // Services.mm.loadFrameScript("resource://tracking-protection-study/tracking-study-content.js", true);
+    // Feature.init();
+    // Start up your feature, with specific variation info.
+    this.feature = new Feature({variation, studyUtils, reasonName: REASONS[reason]});
+  },
+
+  /** addon_install ONLY:
+  * - note first seen,
+  * - check eligible
+  */
+  async isEligible() {
+    //  telemetry "enter" ONCE
+    studyUtils.firstSeen();
+    const isEligible = await config.isEligible(); // addon-specific
+    if (!isEligible) {
+      // 1. uses config.endings.ineligible.url if any,
+      // 2. sends UT for "ineligible"
+      // 3. then uninstalls addon
+      await studyUtils.endStudy({reason: "ineligible"});
+    }
+    return isEligible;
+  },
+
+  async startupWebExtension(webExtension) {
+    webExtension.startup().then(api => {
+      const {browser} = api;
+      /** spec for messages intended for Shield =>
+        * {shield:true,msg=[info|endStudy|telemetry],data=data}
+        */
+      browser.runtime.onMessage.addListener(studyUtils.respondToWebExtensionMessage);
+      // other browser.runtime.onMessage handlers for your addon, if any
+    });
+  },
+
+  // choose the variation for this particular user, then set it.
+  async selectVariation() {
+    const variation = this.getVariationFromPref(config.weightedVariations) ||
+      await studyUtils.deterministicVariation(config.weightedVariations);
+    studyUtils.setVariation(variation);
+    log.debug(`studyUtils has config and variation.name: ${variation.name}.  Ready to send telemetry`);
+    return variation;
   },
 
   // helper to let Dev or QA set the variation name
@@ -156,15 +191,6 @@ this.Bootstrap = {
       return variation;
     }
     return name; // undefined
-  },
-
-  // logging for bootstrap.js, pref sets how verbose
-  setupLog() {
-    const PREF_LOGGING_LEVEL = "shield.testing.logging.level";
-    const BOOTSTRAP_LOGGER_NAME = `shield-study-${config.study.studyName}`;
-    this.log = Log.repository.getLogger(BOOTSTRAP_LOGGER_NAME);
-    this.log.addAppender(new Log.ConsoleAppender(new Log.BasicFormatter()));
-    this.log.level = Services.prefs.getIntPref(PREF_LOGGING_LEVEL, Log.Level.Warn);
   },
 };
 
