0b62fec5a0d366d696de77cf155c091db1b0c92c
Bianca Danforth <biancadanforth@gmail.com>
2018-02-12 17:07:45 -0800

    Add local Firefox testing helper script. Fixes #91.
    Replaced 'mach-test.sh' with 'test-in-tree.sh'. The latter now runs with the command 'npm run test', instead of being appended to 'npm run build'.
    
    Right now the path to mozilla-unified and which test to run locally is hardcoded into 'test-in-tree.sh'; there should we an easy way to edit this for developers.

--- bin/mach-test.sh
+++
@@ -1,8 +0,0 @@
-# Copying build-includes into dist/${ADDON_NAME}/ for testing in tree
-BASE_DIR="$(dirname "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)")"
-ADDON_NAME=$(node -p -e "require('./package.json').name");
-mkdir -p dist/$ADDON_NAME
-while read -r LINE || [[ -n "${LINE}" ]]; do
-  mkdir -p "$(dirname "dist/${ADDON_NAME}/${LINE}")"
-  cp -r "${BASE_DIR}/addon/${LINE}" "$(dirname "dist/${ADDON_NAME}/${LINE}")"
-done < "${BASE_DIR}/build-includes.txt"
